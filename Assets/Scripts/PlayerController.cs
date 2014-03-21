85f172f801999312266e082972c068da86daaf1f
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-21 18:31:20 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+
+	private int count;
+
+	void Start(){
+		count = 0;
+		winText.text = "";
+		UpdateCountText();
+	}
+
+	void FixedUpdate ()
+	{
+		var moveHorizontal = Input.GetAxis ("Horizontal");
+		var moveVertical = Input.GetAxis ("Vertical");
+
+		var movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	private void UpdateCountText(){
+		countText.text = "Count: " + count;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "PickUp"){
+			other.gameObject.SetActive(false);
+			count++;
+			UpdateCountText();
+		}
+
+		if(count >= 9)
+			winText.text = "You win!";
+	}
+}
